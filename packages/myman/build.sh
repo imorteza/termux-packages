0d0e2cf0a23e9beaff696d642d7b171b6f056168
Hax4us <lkpandey950@gmail.com>
2018-04-07 11:02:23 +0530

    update build.sh

--- packages/myman/build.sh
+++ packages/myman/build.sh
@@ -5,3 +5,6 @@ TERMUX_PKG_MAINTAINER="lokesh @hax4us"
 TERMUX_PKG_DEPENDS="ncurses"
 TERMUX_PKG_SHA256=bf69607eabe4c373862c81bf56756f2a96eecb8eaa8c911bb2abda78b40c6d73
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/myman/myman-cvs/myman-cvs-2009-10-30/myman-wip-2009-10-30.tar.gz
+termux_step_post_configure() {
+ export CFLAGS="-Os"
+}
