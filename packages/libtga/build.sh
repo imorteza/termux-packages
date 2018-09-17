d95cba334898203449f04ff72c6c7b0b8f0692e3
Hax4us <lkpandey950@gmail.com>
2018-09-17 22:54:58 +0530

    add libtga

---
+++ packages/libtga/build.sh
@@ -0,0 +1,6 @@
+TERMUX_PKG_HOMEPAGE=http://tgalib.sourceforge.net
+TERMUX_PKG_DESCRIPTION="Libtga is a free (GNU LGPL) and small library to read and write TGA images."
+TERMUX_PKG_VERSION=1.0.1
+TERMUX_PKG_MAINTAINER="lokesh @hax4us"
+TERMUX_PKG_SHA256=038ff0dad25ae97cd7ece547bfab5d0328945d2c8c24c3ade75f682988797be3
+TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/tgalib/libtga/${TERMUX_PKG_VERSION}/libtga-${TERMUX_PKG_VERSION}.tar.gz
