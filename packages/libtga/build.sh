5284164822ee20d710bcc41bf9f1a3c13e3393c7
Hax4us <lkpandey950@gmail.com>
2018-09-17 23:10:27 +0530

    update

--- packages/libtga/build.sh
+++ packages/libtga/build.sh
@@ -4,3 +4,4 @@ TERMUX_PKG_VERSION=1.0.1
 TERMUX_PKG_MAINTAINER="lokesh @hax4us"
 TERMUX_PKG_SHA256=038ff0dad25ae97cd7ece547bfab5d0328945d2c8c24c3ade75f682988797be3
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/tgalib/libtga/${TERMUX_PKG_VERSION}/libtga-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_BUILD_IN_SRC=yes
